770e77253eb09faf51697b778e9607cfa9a2b3fc
wallis <wallismark@gmail.com>
2023-10-21 18:22:35 +0200

    established database connection

---
+++ src/main/java/com/first/usermgntsystem/UserMgntSystemApplication.java
@@ -0,0 +1,13 @@
+package com.first.usermgntsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgntSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgntSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/usermgntsystem/UserMgntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.usermgntsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgntSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
